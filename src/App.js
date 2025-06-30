798fdb084ce68d1aef05176139dd34d8a2aa00a9
Raghav-don <raghavendra0417k@gmail.com>
2025-06-30 12:35:36 +0530

    finish

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import React, { useState } from 'react';
 function App() {
   const [darkMode, setDarkMode] = useState(false);
   const [text, setText] = useState("Click the button to see the text");
+  const [name,setName]=useState("");
+
 
   const showText = () => {
     setText("Hello! You clicked the button.From this you have learned how to use react function components,eventListener,hook");
@@ -19,7 +21,20 @@ const clearText=()=>{
       <br></br>
       <br></br>
       <h1>Click Counter App</h1>
-      <Counter />
+      <br>
+      </br>
+      <input
+      type="text"
+      placeholder="Enter your name"
+      value={name}
+      onChange={(e)=> setName(e.target.value)}
+      />
+      <br>
+      </br>
+      <p>Hello ,{name ? name:"User"}</p>
+      <br>
+      </br>
+      
       <button className="btn-theme" onClick={() => setDarkMode(!darkMode)}>
         {darkMode ? "Switch to Light Mode" : "Switch to Dark Mode"}
       </button>
@@ -32,6 +47,8 @@ const clearText=()=>{
         Hide Message
       </button>
       <p>{text}</p>
+      
+      <Counter />
     </div>
   );
 }
